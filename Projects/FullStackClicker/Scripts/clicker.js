23aa406f6c5628be0054aac65cb417a78dcca016
Bogdan Gura <bogdan.gura@stu.cvccworks.edu>
2024-01-30 19:33:40 -0500

    Cool down functionality completed (Only for Pizza form project)

--- Projects/FullStackClicker/Scripts/clicker.js
+++ Projects/FullStackClicker/Scripts/clicker.js
@@ -7,6 +7,7 @@ let moneyScoreElement = document.getElementById("moneyScoreElement");
 let xpScoreElement = document.getElementById("xpScoreElement");
 let storeContainer = document.querySelector(".store-container");
 let infoContainer = document.querySelector(".info-container");
+let actionContainer = document.querySelector(".actions-menu-container");
 
 //Sound Elements
 let xpClickSound = document.getElementById("clickSoundXP");
@@ -17,18 +18,34 @@ let clickSoundPurchase = document.getElementById("clickSoundPurchase");
 let minutes = 20;
 let minutesInSeconds = minutes * 60;
 
+//Cooldown times for projects (seconds)
+const pizzaFormCooldownOriginal = 30;
+const toDoListCooldownOriginal = 60;
+const frogPuzzleCooldownOriginal = 120;
+let pizzaFormCooldown = pizzaFormCooldownOriginal;
+let toDoListCooldown = toDoListCooldownOriginal;
+let frogPuzzleCooldown = frogPuzzleCooldownOriginal;
+
+//Intervals for each upgrade
+let pizzaFormInterval;
+let toDoListInterval;
+let frogPuzzleInterval;
+
 //Score elements
 let moneyScore = 0;
 let xpScore = 0;
 
 //Earning Rates for jobs and assets (money)
-let fastFoodRate = 8;
+let fastFoodRate = 200;
 
 //Earning rates for xp
-let xpEarningRate = 0;
+let xpEarningRate = 200;
 
 //Upgrade names array 
 let upgradeImgNames = ["CSS/HTML Book", "JS Book Lvl 1"];
+
+//Practice projects names
+let projectNames = ["Pizza-Order-Form", "Dynamic-Todo-list", "Frog-Puzzle"];
 
 //Upgrade objects
 let cssHTMLBook = {
@@ -36,8 +53,10 @@ let cssHTMLBook = {
     description: "A begginer's guide into the world of web development. (Allows to Earn XP)",
     moneyPrice: 60,
     xpPrice: 0,
+    specialRequirments: "NONE",
     bought: false,
     xpBonus: 5,
+    gives: "Allows player to earn xp and adds 5 xp per click to the players xp rate",
     effects: () =>{
         //Subtract the bying cost first
         moneyScore-=cssHTMLBook.moneyPrice;
@@ -55,6 +74,9 @@ let cssHTMLBook = {
 
         //Play the purchase sound
         clickSoundPurchase.play();
+
+        //Clear the infoContainer of old info
+        removeInfo();
     }
 }
 
@@ -63,8 +85,10 @@ let jsBookLvl1 = {
     description: "A begginer's guide to Java Script. (Allows to Earn XP)",
     moneyPrice: 100,
     xpPrice: 0,
+    specialRequirments: "NONE",
     bought: false,
     xpBonus: 10,
+    gives: "Allows player to earn xp and adds 10 xp per click to the players xp rate",
     effects: () =>{
         //Subtract the bying cost first
         moneyScore-=jsBookLvl1.moneyPrice;
@@ -82,6 +106,51 @@ let jsBookLvl1 = {
 
         //Play the purchase sound
         clickSoundPurchase.play();
+
+        //Clear the infoContainer of old info
+        removeInfo();
+    }
+}
+
+//Projects objects
+
+let pizzaForm = {
+    name: "Pizza-Order-Form",
+    description: "A simple form to order a pizza",
+    moneyPrice: 0,
+    xpPrice: 100,
+    specialRequirments: "CSS/HTML Book",
+    bought: false,
+    xpBonus: 500,
+    gives: "5x players XP investment",
+    numPurchased: 0,
+    effects: () =>{
+        let quantityField = document.querySelector(".Pizza-Order-Form h1");
+
+        //Subtract the bying cost first
+        moneyScore-=pizzaForm.moneyPrice;
+        xpScore-=pizzaForm.xpPrice;
+
+        //Set the bought to true
+        pizzaForm.bought = true;
+
+        //Update the xpEarningRate
+        xpScore += pizzaForm.xpBonus;
+
+        //increment the numPurchased
+        pizzaForm.numPurchased++;
+
+        //Update the counters information
+        moneyScoreElement.innerText = moneyScore;
+        xpScoreElement.innerText = xpScore;
+        quantityField.innerText = pizzaForm.numPurchased;
+
+        //Play the purchase sound
+        //clickSoundPurchase.play();
+
+        //Pass the project element to the function that will
+        //give a different styling and start the cooldown timer
+        startCooldown(projects[0], pizzaForm.name, pizzaFormCooldown);
     }
 }
 
@@ -112,7 +181,10 @@ function createListeners()
         console.log(`XP Score: ${moneyScore}`);
     });
 
-    storeUpgradesList = document.querySelectorAll(".store-container div");
+    //List for all the items needing in an
+    //event listener
+    let storeUpgradesList = document.querySelectorAll(".store-container div");
+    let projectsList = document.querySelectorAll(".actions-menu-container div");
 
     console.log(storeUpgradesList);
 
@@ -136,12 +208,37 @@ function createListeners()
         element.addEventListener("mouseleave", removeInfo);
     });
 
+    //Listeners for projects
+    projectsList[0].addEventListener("click", () => {
+        buyUpgrade("Pizza-Order-Form", projectsList[0])
+    });
+
+    // projectsList[1].addEventListener("click", () => {
+    //     buyUpgrade("Dynamic Todo List", projectsList[1])
+    // });
+    
+    // projectsList[2].addEventListener("click", () => {
+    //     buyUpgrade("Frog Puzzle", projectsList[2])
+    // });
+
+    //Tooltip listeners
+    projectsList.forEach(element =>{
+        element.addEventListener("mouseenter", generateInfo);
+    });
+
+    projectsList.forEach(element =>{
+        element.addEventListener("mouseleave", removeInfo);
+    });
+
 }
 
 function startGame()
 {
     //Fill the store container with upgrades
     generateStoreItems();
+
+    //Fill the projects variable
+    projects = document.querySelectorAll(".project");
 
     createListeners();
 
@@ -186,9 +283,83 @@ function updateCountDown()
     minutesInSeconds--;
 }
 
+//StartCount starts the countdown by first
+//applying cooldown styling to the element
+//and then starting an interval with a function
+//that will update the cooldown number until it is zero then the 
+//interval is cleared and styling 
+function startCooldown(projectElement, projectName, cooldownTime) {
+    // Interval for interval clear
+    let interval;
+
+    // Swap styling
+    projectElement.classList.remove("project");
+    projectElement.classList.add("project-cooldown");
+
+    // Append child with the seconds inside
+    let coolDownTimer = document.createElement("p");
+    coolDownTimer.classList.add("coolDownDigits");
+    coolDownTimer.innerText = `${cooldownTime} seconds`;
+    projectElement.appendChild(coolDownTimer);
+
+    // Set an interval that will reduce the number of seconds
+    //which interval (variable)
+    switch (projectName) 
+        {
+            case "Pizza-Order-Form":
+                pizzaFormInterval = setInterval(() => manageCooldown(coolDownTimer, projectName, projectElement), 1000);
+                break;
+        }
+}
+
+//manageCooldown that will accept the element and the 
+//cooldown time
+function manageCooldown(coolDownElement, projectName, projectElement)
+{
+    //check if the cooldown is over or not
+    if(pizzaFormCooldown === 0)
+    {
+        //reset the cooldown timer (depending on the name)
+        switch (projectName) 
+        {
+            case "Pizza-Order-Form":
+                pizzaFormCooldown = pizzaFormCooldownOriginal;
+                break;
+        }
+
+        //Using switch structure determine what interval to clear
+        switch (projectName) 
+        {
+            case "Pizza-Order-Form":
+                clearInterval(pizzaFormInterval);
+                //The remove the project-cooldown class
+                //and apply project instead
+                //and remove the coolDownElement 'p' tag
+                projectElement.classList.remove("project-cooldown");
+                projectElement.classList.add("project");
+                coolDownElement.remove();
+                break;
+        }
+    }
+    //if not over, reduce the coldownTime
+    //Then update the value of the coolDownElement
+    else{
+        switch (projectName) 
+        {
+            case "Pizza-Order-Form":
+                pizzaFormCooldown--;
+                coolDownElement.innerText = "";
+                coolDownElement.innerText = `${pizzaFormCooldown} seconds`;
+                break;
+        }
+    }
+}
+
 //Generates upgardes in the store container
+//and practice projects
 function generateStoreItems()
 {
+    //Generate Upgrades
     for (let i = 0; i < upgradeImgNames.length; i++) 
     {
         //Apends generated divs to the store container
@@ -203,6 +374,27 @@ function generateStoreItems()
 
         //Attach the div to the container
         storeContainer.appendChild(storeItem);
+    }
+
+    //Generate Projects
+    for (let i = 0; i < 1; i++) 
+    {
+        //Apends generated divs to the store container
+        let projectItem = document.createElement("div");
+        let projectItemImg = document.createElement("img");
+        let quantity = document.createElement("h1");
+        quantity.innerText = "0";
+        projectItem.classList.add("project", projectNames[i]);
+
+        //Put the img inside the div and set its alt to the arrays
+        //index value
+        projectItemImg.alt = projectNames[i];
+        projectItem.name = projectNames[i];
+        projectItem.appendChild(projectItemImg);
+        projectItem.appendChild(quantity);
+
+        //Attach the div to the container
+        actionContainer.appendChild(projectItem);
     }
 }
 
@@ -225,6 +417,10 @@ function buyUpgrade(itemName, itemElement)
                 jsBookLvl1.effects();
                 itemElement.remove();
                 break;
+
+            case "Pizza-Order-Form":
+                pizzaForm.effects();
+                break;
         }
     }
 }
@@ -233,6 +429,7 @@ function buyUpgrade(itemName, itemElement)
 //check depends on the parameter passed
 function canAfford(itemName)
 {
+    //Upgrades
     if(itemName === "CSS/HTML Book")
     {
         //check for sufficient funds and xp
@@ -241,14 +438,7 @@ function canAfford(itemName)
         {
             return true;
         }
-        else if(xpScore < cssHTMLBook.xpPrice)
-        {
-            // alert(`Not enogh XP to purchase ${itemName}`);
-            return false;
-        }
-        else if(moneyScore < cssHTMLBook.moneyPrice)
-        {
-            // alert(`Not enogh funds to purchase ${itemName}`);
+        else{
             return false;
         }
     }
@@ -260,17 +450,25 @@ function canAfford(itemName)
          {
              return true;
          }
-         else if(xpScore < jsBookLvl1.xpPrice)
-         {
-            //  alert(`Not enogh XP to purchase ${itemName}`);
-             return false;
-         }
-         else if(moneyScore < jsBookLvl1.moneyPrice)
-         {
-            //  alert(`Not enogh funds to purchase ${itemName}`);
-             return false;
-         }
+         else{
+            return false;
+        }
     }
+
+    //Projects
+    if(itemName === "Pizza-Order-Form")
+    {
+        if(moneyScore >= pizzaForm.moneyPrice && 
+           xpScore >= pizzaForm.xpPrice &&
+           cssHTMLBook.bought === true)
+         {
+             return true;
+         }
+         else{
+            return false;
+        }
+    }
+
 }
 
 //canAffordVIndicator what will switch styling 
@@ -295,12 +493,12 @@ function canAffordColorIndicator(boolean)
 //with a boolean expression and a color 
 function generateInfo(event)
 {
-    console.log(event);
-
     //let upgrade selected
     let upgradeSelected;
 
     //Cheking what upgrade triggered the mouseenter event
+
+    //Upgrades
     if(event.target.name === "CSS/HTML Book")
     {
         upgradeSelected = cssHTMLBook;
@@ -310,6 +508,14 @@ function generateInfo(event)
         upgradeSelected = jsBookLvl1;
     }
 
+    //Projects
+    if(event.target.name === "Pizza-Order-Form")
+    {
+        upgradeSelected = pizzaForm;
+        console.log("Pizza form selected");
+    }
+
+
     //Using upgrade selected generate inner html and 
     //pack it inside of the div
     infoContainer.innerHTML = generateInnerHTMLToolTip(upgradeSelected);
@@ -318,10 +524,12 @@ function generateInfo(event)
 //Generates inner HTML for tool tip
 function generateInnerHTMLToolTip(upgrade)
 {
+    console.log(upgrade);
     let innerHTML = `<p>${upgrade.description}</p>
         <p>Money price: <span class='money'>${upgrade.moneyPrice}</span></p>
         <p>XP price: <span class='xp'>${upgrade.xpPrice}</span></p>
-        <p>Effects: Allows to earn ${upgrade.xpBonus} experience more per click</p>
+        <p>Special Requirements: ${upgrade.specialRequirments}</p>
+        <p>Effects: ${upgrade.gives}</p>        
         <p>Can afford: <span style='color:${canAffordColorIndicator(canAfford(upgrade.name))}'>${canAfford(upgrade.name)}</span></p>`;
 
         return innerHTML;
@@ -332,3 +540,4 @@ function removeInfo()
 {
     infoContainer.innerHTML = "";
 }
+
