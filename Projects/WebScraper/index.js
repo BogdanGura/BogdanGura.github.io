da9ad887b67323719ce9b9f20cf90789f89b0d28
Bogdan Gura <bogdan.gura@stu.cvccworks.edu>
2024-02-08 17:07:19 -0500

    Web Scraper functionality done

---
+++ Projects/WebScraper/index.js
@@ -0,0 +1,26 @@
+//Bogdan Gura
+const puppeteer = require('puppeteer');
+
+(async () => {
+  //Browser variable
+  const browser = await puppeteer.launch({headless: false});
+
+  //open new page in the browser
+  const page = await browser.newPage();
+
+  //go to my portfolio
+  await page.goto("https://bogdangura.github.io/");
+
+  //Take a screen shot of the page 
+  await page.screenshot({path: "Projects/WebScraper/ScreenShots/myWebsite.png"});
+
+  const grabPhoto = await page.evaluate(() => {
+    const image = document.getElementById("personal-img");
+    return image;
+  })
+
+  console.log(grabPhoto);
+
+  //close browser
+  await browser.close();
+})();
